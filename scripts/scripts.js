0d607d0cbea1de35852df9b0851e61940e3c4098
Matheus <matheus.augustoreal@gmail.com>
2022-01-27 22:50:32 -0300

    Create style.css anda script.js

---
+++ scripts/scripts.js
@@ -0,0 +1,47 @@
+const FRONT = "card-front"
+const BACK = "card-back"
+
+let techs = [
+    "bootstrap",
+    "css",
+    "electron",
+    "firebase",
+    "html",
+    "javascript",
+    "jquery",
+    "mongo",
+    "node",
+    "react"
+]
+
+createIdWithTech(techs)
+
+function createCardsFromTechs(techs) {
+    
+    let cards = []
+
+    for(let tech of techs) {
+        cards.push(createPairFromTech(tech))
+    }
+
+    return cards.flatMap(pair => pair)
+
+}
+
+function createPairFromTech(tech) {
+
+    return [{
+        id: createIdWithTech(tech),
+        icon: tech,
+        flipped: false,
+    }, {
+        id: createIdWithTech(tech),
+        icon: tech,
+        flipped: false,
+    }]
+
+}
+
+function createIdWithTech(tech) {
+    return tech + parseInt(Math.random() * 1000)
+}
