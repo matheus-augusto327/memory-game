2e0388b963f61588c893e0455ef1dfa5148dca20
Matheus <matheus.augustoreal@gmail.com>
2022-01-31 21:20:23 -0300

    Created game over and restart functions

--- scripts/game.js
+++ scripts/game.js
@@ -3,6 +3,19 @@ let game = {
     lockMode: false,
     firstCard: null,
     secondCard: null,
+    
+    techs: [
+        "bootstrap",
+        "css",
+        "electron",
+        "firebase",
+        "html",
+        "javascript",
+        "jquery",
+        "mongo",
+        "node",
+        "react"
+    ],
 
     setCard: function(id) {
         let card = this.cards.filter(card => card.id === id)[0]
@@ -43,18 +56,9 @@ let game = {
         this.clearCards()
     },
 
-    techs: [
-        "bootstrap",
-        "css",
-        "electron",
-        "firebase",
-        "html",
-        "javascript",
-        "jquery",
-        "mongo",
-        "node",
-        "react"
-    ],
+    checkGameOver() {
+        return this.cards.filter(card => !card.flipped).length == 0
+    },
 
     cards: null,
 
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,7 +11,7 @@ function startGame() {
 
 function initializeCards(cards) {
     let gameBoard = document.getElementById("gameBoard")
-
+    gameBoard.innerHTML = ""
     game.cards.forEach(card => {
 
         let cardElement = document.createElement('div');
@@ -58,6 +58,10 @@ function flipCard() {
         if(game.secondCard) {
             if(game.checkMatch()) {
                 game.clearCards()
+                if(game.checkGameOver()) {
+                    let gameOverLayer = document.getElementById("gameOver")
+                    gameOverLayer.style.display = "flex"
+                }
             } else {
                 setTimeout(() => {
                     let firstCardView = document.getElementById(game.firstCard.id)
@@ -70,4 +74,11 @@ function flipCard() {
             }
         }
     }
+}
+
+function restart() {
+    game.clearCards()
+    startGame()
+    let gameOverLayer = document.getElementById("gameOver")
+    gameOverLayer.style.display = "none"
 }
